3efbda9ca596082c082f471ac81b9501e5f8360b
Andre Patacas <guerreiroapatacas@gmail.com>
2020-10-22 17:09:23 +0100

    first module completed

--- src/grp_src/grp_mksofs/grp_mksofs_FRT.cpp
+++ src/grp_src/grp_mksofs/grp_mksofs_FRT.cpp
@@ -10,15 +10,19 @@
 namespace sofs20 {
 void grpFillReferenceTable(uint32_t ntotal, uint32_t itotal, uint32_t dbtotal) {
   soProbe(605, "%s(%u, %u, %u)\n", __FUNCTION__, ntotal, itotal, dbtotal);
-
+  //binFillReferenceTable(ntotal, itotal, dbtotal);
+  //return;
   /* replace the following line with your code */
   //binFillReferenceTable(ntotal, itotal, dbtotal);
-  uint32_t rttotal = ntotal - itotal - dbtotal;
+  uint32_t rttotal = ntotal - itotal / IPB - dbtotal - 1;
   uint32_t ref[RPB];
-  for (int b = 0; b < rttotal; b++) {
+  for (uint b = 0; b < rttotal; b++) {
     memset(ref, BlockNullReference, BlockSize);
-    for (int i = 0; i < RPB; i++) {
-      ref[i] = i + RPB * b + 69;
+    for (uint i = 0; i < RPB; i++) {
+      if (i + RPB * b + 69 < dbtotal)
+        ref[i] = i + RPB * b + 69;
+      else
+        ref[i] = BlockNullReference;
     }
     soWriteRawBlock(ntotal - rttotal + b, ref);
   }
