ec10f3160b571b3ae85daedfc7b11d5ab6041d48
Andre Patacas <guerreiroapatacas@gmail.com>
2020-10-25 12:39:29 +0000

    fill superblock bug fixes and start of alloc inode

--- tests/mksofs/fill_superblock.sh
+++ tests/mksofs/fill_superblock.sh
@@ -3,14 +3,15 @@
 # $3 = block_range_start
 # $4 = block_range_end
 source ./mksofs/test_function.sh
-test_function 601 4 0 0 -s
-test_function 601 20 0 0 -s
-test_function 601 50 0 0 -s
-test_function 601 100 0 0 -s
-test_function 601 10000 0 0 -s
-test_function 601 1000 0 0 -s
-test_function 601 2000 0 0 -s
+test_function 602 4 0 0 -s
+test_function 602 10 0 0 -s
+test_function 602 20 0 0 -s
+test_function 602 50 0 0 -s
+test_function 602 100 0 0 -s
+test_function 602 10000 0 0 -s
+test_function 602 1000 0 0 -s
+test_function 602 2000 0 0 -s
 
-test_function 601 4 0 3 -x -z
-test_function 601 20 0 19 -x -z
-test_function 601 1000 0 999 -x -z
+test_function 602 4 0 3 -x -z
+test_function 602 20 0 19 -x -z
+test_function 602 1000 0 999 -x -z
