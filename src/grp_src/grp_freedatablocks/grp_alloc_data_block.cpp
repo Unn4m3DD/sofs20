08ba6c81879feddf0fb42679dd07491cb07ef4c7
BearkillerPT <gilteixeira@ua.pt>
2020-10-30 19:40:10 +0000

    allocDB and freeDB

--- src/grp_src/grp_freedatablocks/grp_alloc_data_block.cpp
+++ src/grp_src/grp_freedatablocks/grp_alloc_data_block.cpp
@@ -3,27 +3,33 @@
  *  \authur Artur Pereira - 2009-2020
  */
 
-#include "freedatablocks.h"
-
-#include <stdio.h>
 #include <errno.h>
 #include <inttypes.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
-#include <sys/types.h>
 
 #include "core.h"
-#include "devtools.h"
 #include "daal.h"
+#include "devtools.h"
+#include "freedatablocks.h"
 
-namespace sofs20
-{
-    uint32_t grpAllocDataBlock()
-    {
-        soProbe(441, "%s()\n", __FUNCTION__);
+namespace sofs20 {
+uint32_t grpAllocDataBlock() {
+  soProbe(441, "%s()\n", __FUNCTION__);
 
-        /* replace the following line with your code */
-        return binAllocDataBlock();
-    }
-};
-
+  /* replace the following line with your code */
+  //return binAllocDataBlock();
+  SOSuperblock* sb = soGetSuperblockPointer();
+  if (sb->dbfree == 0) throw SOException(ENOSPC, __FUNCTION__);
+  if (sb->retrieval_cache.idx == REF_CACHE_SIZE - 1)
+    soReplenishRetrievalCache();
+  int idx = 0;
+  for (;sb->retrieval_cache.ref[idx] == BlockNullReference; idx++);
+  int res = sb->retrieval_cache.ref[idx];
+  sb->retrieval_cache.ref[idx] = BlockNullReference;
+  soSaveSuperblock();
+  return res;
+}
+};  // namespace sofs20
--- src/grp_src/grp_freedatablocks/grp_free_data_block.cpp
+++ src/grp_src/grp_freedatablocks/grp_free_data_block.cpp
@@ -3,36 +3,34 @@
  *  \authur Artur Pereira - 2016-2020
  */
 
-#include "freedatablocks.h"
-
-#include <stdio.h>
 #include <errno.h>
 #include <inttypes.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
-#include <sys/types.h>
 
 #include "core.h"
-#include "devtools.h"
 #include "daal.h"
+#include "devtools.h"
+#include "freedatablocks.h"
 
-namespace sofs20
-{
-    void grpFreeDataBlock(uint32_t bn)
-    {
-        soProbe(442, "%s(%u)\n", __FUNCTION__, bn);
+namespace sofs20 {
+void grpFreeDataBlock(uint32_t bn) {
+  soProbe(442, "%s(%u)\n", __FUNCTION__, bn);
 
-        /* replace the following line with your code */
-        //binFreeDataBlock(bn);
-        SOSuperblock* sb = soGetSuperblockPointer();
-        if(bn < 0 || sb-> dbtotal <= bn) throw SOException(EINVAL, __FUNCTION__);
-        if(sb->insertion_cache.idx == REF_CACHE_SIZE - 1)
-            soDepleteInsertionCache();
-        for(int i = 0; i < REF_CACHE_SIZE; i++) 
-            if(sb->insertion_cache.ref[i] == 0xFF) {
-                
-                break;
-            }
+  /* replace the following line with your code */
+  
+  SOSuperblock* sb = soGetSuperblockPointer();
+  if (bn < 0 || sb->dbtotal <= bn) throw SOException(EINVAL, __FUNCTION__);
+  if (sb->insertion_cache.idx == REF_CACHE_SIZE - 1)
+    soDepleteInsertionCache();
+  for (int idx = 0; idx < REF_CACHE_SIZE; idx++)
+    if (sb->insertion_cache.ref[idx] == BlockNullReference) {
+      sb->insertion_cache.ref[idx] = bn;
+      break;
     }
-};
-
+  sb->dbfree++;
+  soSaveSuperblock();
+}
+};  // namespace sofs20
