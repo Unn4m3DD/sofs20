dbb84fa2d5fa9f41cbea9085e5920e6eb9f70f5e
Andre Patacas <guerreiroapatacas@gmail.com>
2020-11-02 17:39:55 +0000

    get_fileblock implemented

--- src/grp_src/grp_fileblocks/grp_get_fileblock.cpp
+++ src/grp_src/grp_fileblocks/grp_get_fileblock.cpp
@@ -1,3 +1,4 @@
+#include <alloca.h>
 #include <errno.h>
 
 #include "core.h"
@@ -15,23 +16,25 @@ uint32_t grpGetFileBlock(int ih, uint32_t fbn) {
   SOInode* current_inode = soGetInodePointer(ih);
   if (fbn < N_DIRECT)
     return current_inode->d[fbn];
-  else if (fbn - N_DIRECT < N_INDIRECT)
+  else if (fbn - N_DIRECT < N_INDIRECT * RPB)
     return grpGetIndirectFileBlock(current_inode, fbn - N_DIRECT);
-  else if (fbn - N_DIRECT - N_INDIRECT < N_DOUBLE_INDIRECT)
+  else if (fbn - N_DIRECT - N_INDIRECT < N_DOUBLE_INDIRECT * RPB * RPB)
     return grpGetDoubleIndirectFileBlock(current_inode, fbn - N_DIRECT - N_INDIRECT);
   throw SOException(EINVAL, __FUNCTION__);
 }
 
 static uint32_t grpGetIndirectFileBlock(SOInode* ip, uint32_t afbn) {
   soProbe(301, "%s(%d, ...)\n", __FUNCTION__, afbn);
-  
-  return 0;
+  uint32_t ref_table[RPB];
+  soReadDataBlock(ip->i1[afbn / RPB], ref_table);
+  return ref_table[afbn % RPB];
 }
 static uint32_t grpGetDoubleIndirectFileBlock(SOInode* ip, uint32_t afbn) {
   soProbe(301, "%s(%d, ...)\n", __FUNCTION__, afbn);
-
-  /* replace the following two lines with your code */
-  throw SOException(ENOSYS, __FUNCTION__);
-  return 0;
+  uint32_t ref_table_first[RPB];
+  soReadDataBlock(ip->i2[afbn / (RPB * RPB)], ref_table_first);
+  uint32_t ref_table[RPB];
+  soReadDataBlock(ref_table_first[afbn / (RPB)], ref_table);
+  return ref_table[afbn % RPB];
 }
 };  // namespace sofs20
