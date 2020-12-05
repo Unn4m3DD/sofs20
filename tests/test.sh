4df6ec3a59f3ade0897c3b68640163ac5561d72f
Andre Patacas <guerreiroapatacas@gmail.com>
2020-12-05 19:16:43 +0000

    report and -h to tests added

--- tests/test.sh
+++ tests/test.sh
@@ -16,6 +16,36 @@ function compile() {
 compile
 rm -f *.log
 touch bin_detect.log
+
+if [[ $1 == "-h" ]]; then
+  echo "mksofs CS"
+  echo "mksofs FIT"
+  echo "mksofs FSB"
+  echo "mksofs FRD"
+  echo "mksofs FRT"
+  echo "mksofs RFDB"
+  echo "------"
+  echo "free_inodes_and_db alloc_inode"
+  echo "free_inodes_and_db alloc_db"
+  echo "free_inodes_and_db free_db"
+  echo "free_inodes_and_db replenish_retrieval"
+  echo "free_inodes_and_db deplete_insertion"
+  echo "------"
+  echo "fileblocks alloc_fb"
+  echo "fileblocks free_fb"
+  echo "fileblocks get_fb"
+  echo "fileblocks write_fb"
+  echo "fileblocks read_fb"
+  echo "------"
+  echo "direntries add_direntry"
+  echo "direntries delete_direntry"
+  echo "direntries check_direntry"
+  echo "direntries get_direntry"
+  echo "direntries rename_direntry"
+  echo "direntries traverse_direntry"
+  echo "------"
+  echo "global"
+fi
 if [[ $1 == "" || $1 == "mksofs" ]]; then
   source mksofs/test.sh 
   test_mksoft $2 $3
@@ -32,8 +62,8 @@ if [[ $1 == "" || $1 == "direntries" ]]; then
   source direntries/test.sh 
   direntries $2 $3
 fi
-if [[ $1 == "" || $1 == "syscalls" ]]; then
-  source syscalls/global_test.sh 
+if [[ $1 == "" || $1 == "global" ]]; then
+  source global/global_test.sh 
 fi
 
 errors="$(cat diff_bin.log)$(cat diff.log)$(cat bin_detect.log)"
